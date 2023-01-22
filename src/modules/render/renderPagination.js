c7dab22f860cce44e742098c63cea7ea71c3e932
Maksim Leskin <maksim.leskin@methed.ru>
2023-01-22 19:12:26 +0300

    favicon & pagination

--- src/modules/render/renderPagination.js
+++ src/modules/render/renderPagination.js
@@ -4,7 +4,7 @@ import { getUrl } from '../utils/getUrl';
 export const renderPagination = (wrapperPagination, page, pages, count) => {
   wrapperPagination.textContent = '';
 
-  createElement(
+  const paginationList = createElement(
     'ul',
     {
       className: 'pagination__list',
@@ -15,7 +15,7 @@ export const renderPagination = (wrapperPagination, page, pages, count) => {
   );
 
   const isNotStart = page - Math.floor(count / 2) > 1;
-  const isEnd = page + Math.floor(count / 2) > pages;
+  const isEnd = page + Math.floor(count / 2) >= pages;
 
   if (count > pages) {
     count = pages
@@ -37,13 +37,15 @@ export const renderPagination = (wrapperPagination, page, pages, count) => {
       {
         className: 'pagination__item',
       },
-      {parent: wrapperPagination,
-      append: createElement('a', {
+      {
+        parent: paginationList,
+        append: createElement('a', {
         textContent: n,
         href:  getUrl({page: n}),
         className: `pagination__link 
           ${page === n ? 'pagination__link_active' : ''}`
-      })}
+        })
+      }
     )
   }
 
@@ -54,10 +56,19 @@ export const renderPagination = (wrapperPagination, page, pages, count) => {
         className: `pagination__arrow pagination__arrow_start
           ${!isNotStart ? 'pagination__arrow_disabled' : ''}`,
         href: getUrl({page: 1}),
-        textContent: 'start',
+        tabIndex: !isNotStart ? '-1' : '0',
+        innerHTML: `
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M14 15.06L10.9096 12L14 8.94L13.0486 8L9 12L13.0486 16L14 15.06Z" fill="currentColor"/>
+          </svg>
+        `,
         ariaLabel: 'В начало'
       },
-      {parent: wrapperPagination},
+      {
+        cb(link) {
+          wrapperPagination.prepend(link)
+        }
+      },
     ),
 
     createElement(
@@ -66,7 +77,12 @@ export const renderPagination = (wrapperPagination, page, pages, count) => {
         className: `pagination__arrow pagination__arrow_end
           ${isEnd ? 'pagination__arrow_disabled' : ''}`,
         href: getUrl({page: pages}),
-        textContent: 'end',
+        tabIndex: isEnd ? '-1' : '0',
+        innerHTML: `
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10 15.06L13.0904 12L10 8.94L10.9514 8L15 12L10.9514 16L10 15.06Z" fill="currentColor"/>
+          </svg>
+        `,
         ariaLabel: 'В конец'
       },
       {parent: wrapperPagination},
